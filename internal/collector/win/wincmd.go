5e5af1a089b91e8876af05c6f9a11c8d7c0f7f26
Elberion <4bd@inbox.ru>
2024-05-03 18:27:16 +1000

    Get CPU win

--- internal/collector/win/wincmd.go
+++ internal/collector/win/wincmd.go
@@ -2,14 +2,33 @@ package win
 
 import (
 	pb "SystemStatDaemon/internal/api/grpc"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 func GetCPU() *pb.CPU {
 	cpu := pb.CPU{
-		UserMode:   10,
-		SystemMode: 3,
-		Idle:       87,
+		UserMode:   0,
+		SystemMode: 0,
+		Idle:       0,
 	}
+	cmd := exec.Command("powershell", "wmic cpu get loadpercentage /value")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		// TODO log
+		return &cpu
+	}
+	str := strings.Trim(string(out[:]), "\n\r")
+	str, _ = strings.CutPrefix(str, "LoadPercentage=")
+	load, err := strconv.Atoi(str)
+	if err != nil {
+		// TODO log
+		return &cpu
+	}
+	cpu.UserMode = float32(load)
+	cpu.Idle = float32(100 - load)
+
 	return &cpu
 }
 
